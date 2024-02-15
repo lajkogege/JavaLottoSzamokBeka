b546a9e611e5832b18af6c6c9ae2238b72eec075
lajkogege <lajkogege@gmail.com>
2024-02-15 11:40:06 +0100

    amoba

--- src/lottoszamok/AmobaJatek.java
+++ src/lottoszamok/AmobaJatek.java
@@ -4,10 +4,22 @@
  */
 package lottoszamok;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author LajkóGergő(SZOFT_202
  */
 public class AmobaJatek {
+    static Scanner sc = new Scanner(System.in);
+    static Random rnd= new Random();
     
+    public static void main(String[] args) {
+        System.out.println("Adjon meg értéket: ");
+        int ertek =sc.nextInt();
+        String [] szimbolumook = new String[9];
+        
+              
+    }
 }
